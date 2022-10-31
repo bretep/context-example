ad1151ceef810bb0f8a002a1b3d3518e27f640b7
Bret Palsson <bretep@gmail.com>
2022-10-31 09:25:14 -0700

    first commit

---
+++ app/components/Home2.svelte
@@ -0,0 +1,32 @@
+<page>
+    <actionBar title="Home" />
+    <stackLayout>
+        <label class="info">
+            <formattedString>
+                <span text=" {$testInformation}" />
+            </formattedString>
+        </label>
+        <button text="Increment" on:tap="{increment}" />
+        <button text="Navigate to next page" on:tap="{increment}" />
+    </stackLayout>
+</page>
+
+<script lang="ts">
+    import {getContext} from "svelte";
+
+    let message: string = "Blank Svelte Native App"
+    const {testInformation, increment} = getContext("test")
+
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
---
+++ app/context/test.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {setContext} from "svelte";
+    import {writable} from "svelte/store";
+    const testInformation = writable("1234")
+
+    setContext("test", testInformation)
+</script>
+<slot/>
